a50b4503ca0e08e82812f6129b076636445a7348
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-01-12 18:01:04 +0100

    Add waiting for DOM

--- js/script.js
+++ js/script.js
@@ -1,38 +1,40 @@
-const templateCarousel = document.getElementById('template-carousel').innerHTML;
-const templateItem = document.getElementById('template-carousel-item').innerHTML;
-Mustache.parse(templateItem);
-let listItems = '';
-for (let i = 0; i < productsData.length; i++) {
-    listItems += Mustache.render(templateItem, productsData[i]);
-}
 
-const fullCarousel = Mustache.render(templateCarousel, {carousel: listItems});
-	
-results.insertAdjacentHTML('beforeend', fullCarousel);
+document.addEventListener("DOMContentLoaded", function(event) {
+    const templateCarousel = document.getElementById('template-carousel').innerHTML;
+    const templateItem = document.getElementById('template-carousel-item').innerHTML;
+    Mustache.parse(templateItem);
+    let listItems = '';
+    for (let i = 0; i < productsData.length; i++) {
+        listItems += Mustache.render(templateItem, productsData[i]);
+    }
+
+    const fullCarousel = Mustache.render(templateCarousel, {carousel: listItems});
+        
+    results.insertAdjacentHTML('beforeend', fullCarousel);
 
 
-const carousel = document.querySelector('.main-carousel');
-const flkty = new Flickity(carousel, {
-    imagesLoaded: true,
-    percentPosition: false,
-    fullscreen: true,
-    cellAlign: 'center',
-    contain: true,
-    hash: true,
-    pageDots: false
+    const carousel = document.querySelector('.main-carousel');
+    const flkty = new Flickity(carousel, {
+        imagesLoaded: true,
+        percentPosition: false,
+        fullscreen: true,
+        cellAlign: 'center',
+        contain: true,
+        hash: true,
+        pageDots: false
 
+    });
+
+    const progressBar = document.querySelector('.progress-bar');
+    flkty.on('scroll', function (progress) {
+        progress = Math.max(0, Math.min(1, progress));
+        progressBar.style.width = progress * 100 + '%';
+    });
+
+
+    document.querySelectorAll('.reset').forEach(function (kind) {
+        kind.addEventListener('click', function (event) {
+            flkty.select(0);
+        });
+    });
 });
-
-const progressBar = document.querySelector('.progress-bar');
-flkty.on('scroll', function (progress) {
-    progress = Math.max(0, Math.min(1, progress));
-    progressBar.style.width = progress * 100 + '%';
-});
-
-
-document.querySelectorAll('.reset').forEach(function (kind) {
-    kind.addEventListener('click', function (event) {
-        flkty.select(0);
-    })
-})
-
