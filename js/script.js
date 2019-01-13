155e48f50f76d7da832aef08e408eb37aa31dff0
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-01-13 11:08:27 +0100

    Zadanie 14.3

--- js/script.js
+++ js/script.js
@@ -1,43 +1,37 @@
+const templateCarousel = document.getElementById('template-carousel').innerHTML;
+const templateItem = document.getElementById('template-carousel-item').innerHTML;
+Mustache.parse(templateItem);
+let listItems = '';
+for (let i = 0; i < productsData.length; i++) {
+    listItems += Mustache.render(templateItem, productsData[i]);
+}
 
-let flkty;
+const fullCarousel = Mustache.render(templateCarousel, { carousel: listItems });
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    const templateCarousel = document.getElementById('template-carousel').innerHTML;
-    const templateItem = document.getElementById('template-carousel-item').innerHTML;
-    Mustache.parse(templateItem);
-    let listItems = '';
-    for (let i = 0; i < productsData.length; i++) {
-        listItems += Mustache.render(templateItem, productsData[i]);
-    }
-
-    const fullCarousel = Mustache.render(templateCarousel, { carousel: listItems });
-
-    results.insertAdjacentHTML('beforeend', fullCarousel);
+results.insertAdjacentHTML('beforeend', fullCarousel);
 
 
-    const carousel = document.querySelector('.main-carousel');
-    flkty = new Flickity(carousel, {
-        imagesLoaded: true,
-        percentPosition: false,
-        fullscreen: true,
-        cellAlign: 'center',
-        contain: true,
-        hash: true,
-        pageDots: false
+const carousel = document.querySelector('.main-carousel');
+let flkty = new Flickity(carousel, {
+    imagesLoaded: true,
+    percentPosition: false,
+    fullscreen: true,
+    cellAlign: 'center',
+    contain: true,
+    hash: true,
+    pageDots: false
 
-    });
+});
 
-    const progressBar = document.querySelector('.progress-bar');
-    flkty.on('scroll', function (progress) {
-        progress = Math.max(0, Math.min(1, progress));
-        progressBar.style.width = progress * 100 + '%';
-    });
+const progressBar = document.querySelector('.progress-bar');
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
+});
 
-
-    document.querySelectorAll('.reset').forEach(function (kind) {
-        kind.addEventListener('click', function (event) {
-            flkty.select(0);
-        });
+document.querySelectorAll('.reset').forEach(function (kind) {
+    kind.addEventListener('click', function (event) {
+        flkty.select(0);
     });
 });
 
@@ -47,7 +41,7 @@ function initMap() {
     for (let i = 0; i < productsData.length; i++) {
         uluru.push(productsData[i].coords);
     };
-    var map = new google.maps.Map(
+    const map = new google.maps.Map(
         document.getElementById('map'), { zoom: 4, center: uluru[0] });
 
     for (let i = 0; i < productsData.length; i++) {
