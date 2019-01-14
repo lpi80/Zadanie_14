32146b0f66692e162a38ef1fa32889f2235bbd6b
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-01-14 12:22:19 +0100

    Add DOMContentLoaded

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
-const carousel = document.querySelector('.main-carousel');
-const flkty = new Flickity(carousel, {
-    imagesLoaded: true,
-    percentPosition: false,
-    fullscreen: true, 
-    cellAlign: 'center',
-    contain: true,
-    hash: true, 
-    pageDots: false
+document.addEventListener("DOMContentLoaded", function(event) {
+    const carousel = document.querySelector('.main-carousel');
+    const flkty = new Flickity(carousel, {
+        imagesLoaded: true,
+        percentPosition: false,
+        fullscreen: true, 
+        cellAlign: 'center',
+        contain: true,
+        hash: true, 
+        pageDots: false
 
-});
+    });
 
-const progressBar = document.querySelector('.progress-bar');
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  progressBar.style.width = progress * 100 + '%';
-});
+    const progressBar = document.querySelector('.progress-bar');
+    flkty.on( 'scroll', function( progress ) {
+    progress = Math.max( 0, Math.min( 1, progress ) );
+    progressBar.style.width = progress * 100 + '%';
+    });
 
 
-document.querySelectorAll('.reset').forEach(function(kind) {
-    kind.addEventListener('click', function(event) {
-        flkty.select(0);
+    document.querySelectorAll('.reset').forEach(function(kind) {
+        kind.addEventListener('click', function(event) {
+            flkty.select(0);
+        })
     })
-})
+});
